db862035539c9f5eac3dd436ae46dd9c61001837
Christopher Grady <grittygrady@gmail.com>
2021-02-24 13:55:49 -0800

    Added more styling

--- src/components/Goal.js
+++ src/components/Goal.js
@@ -1,22 +1,23 @@
 import StyledGoal from '../styles/StyledGoal';
-
+import StyledCheckbox from '../styles/StyledCheckbox';
+import StyledGoalDetails from '../styles/StyledGoalDetails';
 const Goal = ({ goal, updates }) => {
   return (
     <StyledGoal>
-      <label>
+      <StyledCheckbox>
         {' '}
         <input type='checkbox' disabled defaultChecked={goal.fields.complete} />
         <span />
-      </label>
+      </StyledCheckbox>
       <h2>{goal.fields.title}</h2>
-      <div>
+      <StyledGoalDetails>
         <h3>DETAILS</h3>
         <p>{goal.fields.details}</p>
         <h3>UPDATES</h3>
         {updates.map((update, i) => (
           <p key={i}>{update.fields.update}</p>
         ))}
-      </div>
+      </StyledGoalDetails>
     </StyledGoal>
   );
 };
